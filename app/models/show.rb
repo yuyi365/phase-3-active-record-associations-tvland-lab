ce2ab9d01c6dca8a3e076204296378c4700aefa5
Yuyi <yuyi365@gmail.com>
2021-11-30 11:47:51 -0500

    commit tree 09b61f18a35427ee1ecac49ef2c26fe304b4e02b

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors through :characters
+  belongs_to :networks
+
+  def actors_list
+    self.actors.map do |actor|
+        "#{actor.first_name} #{actor.last_name}"
+    end
+  end
+
 end
--- db/migrate/001_create_networks.rb
+++ db/migrate/001_create_networks.rb
@@ -6,10 +6,11 @@
 class CreateNetworks < ActiveRecord::Migration[6.1]
   # define a change method in which to do the migration
   def change
-    create_table :networks do |t| # we get a block variable here for the table
+    create_table :networks do |t|  # we get a block variable here for the table
       # primary key of :id is created for us!
       # defining columns is as simple as t.[datatype] :column
       t.string :call_letters
+
       # the above breaks down to
       # "create a column called :call_letters on table t with type string
       t.integer :channel
